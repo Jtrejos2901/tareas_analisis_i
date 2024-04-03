f0a80097f394fdf544cfac4791a04a8a1ba5efcd
Carolina <caronavamon25@gmail.com>
2024-04-03 11:22:41 -0600

    feat: Se editan gráficos

--- Tarea 2/Carolina.R
+++ Tarea 2/Carolina.R
@@ -13,28 +13,25 @@ beans_ACP <- PCA(beans_datos[,-17], scale.unit=TRUE, ncp=5, graph = FALSE)
 beans_ACP
 
 #b) Eliminar del plano principal los individuos mal representados 
-plot(beans_ACP, axes=c(1, 2), choix="ind",col.ind="blue",new.plot=TRUE,select="cos2 0.1")
+plot(beans_ACP, axes=c(1, 2), choix="ind",col.ind="blue",new.plot=TRUE,select= list(cos2 = 0.1))
 #Eliminar del círculo las variables mal representadas
-plot(beans_ACP, axes=c(1, 2), choix="var",col.var="green",new.plot=TRUE,select="cos2 0.1")
+plot(beans_ACP, axes=c(1, 2), choix="var",col.var= "#6E8B3D",new.plot=TRUE,select= list(cos2 = 0.1))
 
 #f) Convertir Class a código disyuntivo
 
 #Se identifican las categorías de la variable class
-unique(beans_datos$Class)
-
+categorias_class  <- unique(beans_datos$Class)
+categorias_class
 #Se hace código disyuntivo
 
-categorias_class  <- unique(beans_datos$Class)
 lista_disyuntivo  <- list()
-lista_nombres <- list()
-beans_datos2<-beans_datos[,-17]
+beans_datos2<- beans_datos[,-17]
 
-for( i in 1: length(categorias_class)) {
-  lista_nombres[[i]] <-categorias_class[i]
+for (i in 1:length(categorias_class)) {
   lista_disyuntivo[[i]] <- as.numeric(beans_datos$Class == categorias_class[i])
-  lista_nombres$categorias_class[i] <- lista_disyuntivo[[i]] 
+  names(lista_disyuntivo)[i] <- paste("Class_", categorias_class[i], sep = "")
   print(lista_disyuntivo[[i]])
-  beans_datos2 <-cbind(beans_datos2, lista_nombres[[i]])
+  beans_datos2 <- cbind(beans_datos2, lista_disyuntivo[i])
 }
 str(beans_datos2)
 
@@ -43,7 +40,9 @@ beans_ACP2 <- PCA(beans_datos2, scale.unit=TRUE, ncp=5, graph = FALSE)
 beans_ACP2
 
 #Eliminar del plano principal los individuos mal representados 
-plot(beans_ACP2, axes=c(1, 2), choix="ind",col.ind="blue",new.plot=TRUE,select="cos2 0.1")
+plot(beans_ACP2, axes=c(1, 2), choix="ind",col.ind="blue",new.plot=TRUE,select= list(cos2 =0.1))
 #Eliminar del círculo las variables mal representadas
-plot(beans_ACP2, axes=c(1, 2), choix="var",col.var="green",new.plot=TRUE,select="cos2 0.1")
+plot(beans_ACP2, axes=c(1, 2), choix="var",col.var= "#6E8B3D",new.plot=TRUE,select= list(cos2 =  0.1))
+
+
 
