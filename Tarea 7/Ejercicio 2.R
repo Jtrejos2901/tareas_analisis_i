bce1c502c51b5b9fe0ee28ecfefce5186f07e5ed
Tábata Picado Carmona <tabata.picado@ucr.ac.cr>
2024-05-15 12:28:48 -0600

    docs: Se crea la carpeta para la tarea 7 y el Rscript para el ejercicio 2.

---
+++ Tarea 7/Ejercicio 2.R
@@ -0,0 +1,5 @@
+#------------------------------Ejercicio 2--------------------------
+
+# Construya un ejemplo pequeño a pie con 4 variables categóricas que permita 
+# verificar el Teorema 2 visto en clase. Además, compare con el resultado de la 
+# función MCA(....) del paquete FactoMineR.
