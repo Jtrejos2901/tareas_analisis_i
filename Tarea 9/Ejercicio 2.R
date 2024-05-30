0caf05e67d34bb94a2a6a78bbf9aaee004205bc5
Tábata Picado Carmona <tabata.picado@ucr.ac.cr>
2024-05-30 12:05:20 -0600

    Feat: Se avanza con el ejercicio 2.

--- Tarea 9/Ejercicio 2.R
+++ Tarea 9/Ejercicio 2.R
@@ -1,7 +1,13 @@
 library(readr)
 library(cluster)
 library(ggplot2)
+library(dendextend)
+library(patchwork)
+library(factoextra)
+library(dbscan)
 #----------------------------Ejercicio 2----------------------------------------
+# Seembramos la semilla 
+set.seed(26)
 
 # Efectúe un análisis de k-medias realizando los siguientes pasos:
 # a) Cargue la tabla de datos y ejecute un str(...), summary(...) y un dim(...), 
@@ -64,8 +70,6 @@ centro_cluster3 <- data.frame("cluster" = "C3", "variable" = names(centro_cluste
 centros <- rbind(centro_cluster1, centro_cluster2, centro_cluster3)
 for (x in unique(centros$variable)) {
   aux <- centros[centros$variable == x, "valor"]
-  aux <- aux - min(aux)
-  aux <- aux / max(aux)
   centros[centros$variable == x, "valor"] <- aux
 }
 # Cluster 1
@@ -130,8 +134,6 @@ centros_estandarizados <- rbind(centro_estandarizado_cluster1,
                                 centro_estandarizado_cluster3)
 for (x in unique(centros_estandarizados$variable)) {
   aux_estandarizado <- centros_estandarizados[centros_estandarizados$variable == x, "valor"]
-  aux_estandarizado <- aux_estandarizado - min(aux_estandarizado)
-  aux_estandarizado <- aux_estandarizado / max(aux_estandarizado)
   centros_estandarizados[centros_estandarizados$variable == x, "valor"] <- aux_estandarizado
 }
 
@@ -153,10 +155,129 @@ ggplot(centros_estandarizados[centros_estandarizados$cluster == "C3", ],
   geom_bar(stat = "identity", fill = color) +
   labs(x = "Variables", y = "Cluster 3")
 
-# Juntos
+# Juntos estandarizado
 ggplot(centros_estandarizados, aes(x = variable, y = valor, fill = cluster)) + 
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_manual(values = c("pink4", "#FFC0CB", "#FF82AB")) +
   labs(x = "Variables", y = "")
 
-  
+# i) Ejecute un k−medias con k = 3 con iter.max=1000 y nstart=50.
+
+k_medias_2 <- kmeans(datos_estandarizados, centers = 3, iter.max = 1000, 
+                     nstart = 50)
+head(k_medias_2$cluster)  
+k_medias_2$centers
+
+# j) Dé una interpretación de los resultados del punto anterior usando un 
+# gráfico tipo radar.
+
+centros_gravedad_2<- k_medias_2$centers
+
+centro_2_cluster1 <- centros_gravedad_2[1,]
+centro_2_cluster1 <- data.frame("cluster" = "C1", 
+                                            "variable" = names(centro_2_cluster1),
+                                            "valor" = centros_gravedad_2[1,])
+centro_2_cluster2 <- centros_gravedad_2[2,]
+centro_2_cluster2 <- data.frame("cluster" = "C2", 
+                                            "variable" = names(centro_2_cluster2),
+                                            "valor" = centros_gravedad_2[2,])
+centro_2_cluster3 <- centros_gravedad_2[3,]
+centro_2_cluster3 <- data.frame("cluster" = "C3", 
+                                            "variable" = names(centro_2_cluster3),
+                                            "valor" = centros_gravedad_2[3,])
+centros_2 <- rbind(centro_2_cluster1, 
+                                centro_2_cluster2, 
+                                centro_2_cluster3)
+for (x in unique(centros_2$variable)) {
+  aux_2 <- centros_2[centros_2$variable == x, "valor"]
+  aux_2 <- aux_2 - min(aux_2)
+  aux_2 <- aux_2 / max(aux_2)
+  centros_2[centros_2$variable == x, "valor"] <- aux_2
+}
+
+# Gráfico de radar
+
+ggplot(data = centros_2[order(centros_2$variable), ],
+       aes(x = variable, y = valor, group = cluster)) + 
+  geom_point(aes(colour = cluster)) +
+  geom_polygon(aes(colour = cluster, fill = cluster), alpha = 0.2) +
+  ylim(-0.1, 1) + labs(x = "", y = "") + theme_minimal() +
+  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank()) +
+  annotate('text', x = 0, y = c(0, 0.25, 0.5, 0.75, 1),
+           label = c("0%", "25%", "50%", "75%", "100%"), color = "darkgray") +
+  coord_polar()
+
+# k) Observe que si ejecutamos el método k−medoides con k = 3 con nstart=50 con 
+# esta tabla de datos este método nunca termina o tarda demasiado. ¿Por qué 
+# sucede esto?
+
+# l) Ejecute un k−medoides con k = 3 con nstart=50. Para esto tome una muestra 
+# de 5 % de los datos, esto se puede lograr con el siguiente código:
+
+numero_filas <- nrow(datos_estandarizados)
+muestra <- sample(1:numero_filas,numero_filas*0.05)
+datos_muestra <- datos_estandarizados[muestra,]
+modelo_kmd <- pam(datos_muestra, 3, nstart = 50)
+
+# m) Aplique el método dbscan con todos los datos ¿Termina el método? Sino 
+# termina ejecútelo en la muestra del 5 %
+
+# Con esta función podemos encontrar el valor correcto para el eps.
+kNNdistplot(datos_muestra, k = 4)
+
+# No termina de correr con todos los datos.
+metodo_dbscan <- fpc::dbscan(datos_estandarizados, eps = 2, MinPts =4)
+
+# Sí corre con la muestra del 5%.
+metodo_dbscan <- fpc::dbscan(datos_muestra, eps = 2, MinPts =4)
+graf <- fviz_cluster(metodo_dbscan, data = datos_muestra, stand =F,
+                  ellipse = F, show.clust.cent = F,
+                  geom = "point",shape = 19, palette = "Set1",
+                  ggtheme = theme_minimal())
+graf + labs(title = "Agrupamiento basado en densidad") +
+  theme(legend.position = "bottom")
+
+# n) Dé una interpretación de los resultados del punto anterior usando usando un 
+# gráfico tipo radar. ¿Qué diferencias nota respecto a k-medias?
+
+datos_graf_cluster <- datos_muestra
+datos_graf_cluster$cluster <- metodo_dbscan$cluster
+
+# Filtrar las observaciones que pertenecen al cluster 1
+datos_graf_cluster <- datos_graf_cluster[datos_graf_cluster$cluster == 1, ]
+
+# Calcular el centro de gravedad 
+centro_cluster_dbscan <- colMeans(datos_graf_cluster[, -ncol(datos_graf_cluster)])
+
+centro_cluster_dbscan <- data.frame("cluster" = "C1", 
+                                    "variable" = names(centro_cluster_dbscan),
+                              "valor" = centro_cluster_dbscan)
+ggplot(data = centro_cluster_dbscan[order(centro_cluster_dbscan$variable), ],
+       aes(x = variable, y = valor, group = cluster)) + 
+  geom_point(aes(colour = cluster)) +
+  geom_polygon(aes(colour = cluster, fill = cluster), alpha = 0.2) +
+  ylim(-0.1, 1) + labs(x = "", y = "") + theme_minimal() +
+  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank()) +
+  annotate('text', x = 0, y = c(0, 0.25, 0.5, 0.75, 1),
+           label = c("0%", "25%", "50%", "75%", "100%"), color = "darkgray") +
+  coord_polar()
+
+# ñ) Construya el Codo de Jambu usando iter.max=100 y nstart=5, ¿cuántos 
+# conglomerados (clústeres) sugiere el codo? Utilice también el método 
+# silhouette de la función fviz nbclust, ¿cuántos conglomerados (clústeres) 
+# sugiere este método? Para este ejercicio puede utilizar una muestra del 20 % 
+# en caso de limitaciones computacionales.
+
+muestra_20 <- sample(1:numero_filas,numero_filas*0.2)
+datos_muestra_20 <- datos_estandarizados[muestra_20,]
+
+# Codo de Jambu
+InerciaIC<-rep(0,9)
+for(k in 1:9) {
+  grupos <- kmeans(datos_muestra_20, centers=k, nstart=5, iter.max=100)
+  InerciaIC[k]<-grupos$tot.withinss
+}
+plot(InerciaIC,col="blue",type="b")
+
+# Silhouette
+fviz_nbclust(datos_muestra_20, kmeans, method = "silhouette",k.max = 8)
